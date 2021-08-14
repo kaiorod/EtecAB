6775c79146a9163496830542b87646335871856f
kaiorod <caiorodrigo2775@gmail.com>
2021-08-13 23:15:17 -0300

    finalizando programa EtecAB

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a Etec Adolpho Berezin!!");
+        }
+    }
+}
